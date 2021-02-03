a28fcb6887143efc55084838d520526796b93a09
dmb-dev87 <dmbdev800@gmail.com>
2021-02-03 09:32:30 -0500

    [TopOneExpert][2021-02-03][update sip.js library to 0.19.0, change and fix User-Agent value]

---
+++ src/app/utilities/platform/web/end-user/index.ts
@@ -0,0 +1,7 @@
+/**
+ * A simple SIP user implementation for web browsers.
+ * @packageDocumentation
+ */
+export * from './end-user';
+export * from './end-user-delegate';
+export * from './end-user-options';
