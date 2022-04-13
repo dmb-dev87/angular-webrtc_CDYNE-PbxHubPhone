49aaba04810a7d3b01cfe8053ea5e0820fbee2b6
dmb-dev87 <dmbdev800@gmail.com>
2022-04-13 01:19:35 -0400

    [dmb-dev87][2022-04-13][add blind transfer feature]

--- src/app/components/misc-control/misc-control.component.ts
+++ src/app/components/misc-control/misc-control.component.ts
@@ -7,13 +7,16 @@ import { getButtonText } from '../../utilities/ui-utils';
   styleUrls: ['./misc-control.component.scss']
 })
 export class MiscControlComponent implements OnInit {
-  @Output() transfer = new EventEmitter<boolean>();
+  @Output() transferA = new EventEmitter<boolean>();
+  @Output() transferB = new EventEmitter<boolean>();
   @Output() monitor = new EventEmitter();
   @Output() message = new EventEmitter();
   @Output() conference = new EventEmitter<boolean>();
   
-  @Input() xferBtnDisabled: boolean;
-  @Input() transferState: boolean;
+  @Input() axferBtnDisabled: boolean;
+  @Input() bxferBtnDisabled : boolean;
+  @Input() transferStateA: boolean;
+  @Input() transferStateB: boolean;
   @Input() monitorBtnDisabled: boolean;
   @Input() messageBtnDisabled: boolean;
   @Input() receivedMessages: number;
@@ -24,16 +27,28 @@ export class MiscControlComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  onMakeTransfer(): void {
+  onMakeTransferA(): void {
     let completed = false;
-    const btnText = getButtonText('transfer-call').toLowerCase();
-    if (btnText === `x-fer`) {
+    const btnText = getButtonText('transfer-call-a').toLowerCase();
+    if (btnText === `attended x-fer`) {
       completed = false;
     }
     else if (btnText === `complete x-fer`) {
       completed = true;
     }
-    this.transfer.emit(completed);   
+    this.transferA.emit(completed);   
+  }
+
+  onMakeTransferB(): void {
+    let completed = false;
+    const btnText = getButtonText('transfer-call-b').toLowerCase();
+    if (btnText === `blind x-fer`) {
+      completed = false;
+    }
+    else if (btnText === `complete x-fer`) {
+      completed = true;
+    }
+    this.transferB.emit(completed);   
   }
 
   onDialMonitor(): void {
